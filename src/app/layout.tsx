573ee58c25d2733415b91d89f5c9a8f39b372e5a
mailtoneha274-cmd <mailtoneha274@gmail.com>
2026-09-24 15:17:04 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import SmoothScroll from "@/components/SmoothScroll";
 import { BookingModalProvider } from "@/context/BookingModalContext";
 import BookingModal from "@/components/BookingModal";
 import JsonLd from "@/components/JsonLd";
-import Script from "next/script";
+import MetaPixel from "../components/MetaPixel";
 
 const instrumentSerif = Instrument_Serif({
   variable: "--font-instrument-serif",
@@ -117,39 +117,8 @@ export default function RootLayout({
         <JsonLd />
       </head>
       <body className="min-h-full font-sans bg-black text-white selection:bg-[#287417] selection:text-white">
-        export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        {/* Meta Pixel Code */}
-<Script id="meta-pixel" strategy="afterInteractive">
-  {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1648413880066435');
-    fbq('track', 'PageView');
-  `}
-</Script>
-<noscript>
-  {/* eslint-disable-next-line @next/next/no-img-element */}
-  <img
-    height="1"
-    width="1"
-    style={{ display: "none" }}
-    src="https://www.facebook.com/tr?id=1648413880066435&ev=PageView&noscript=1"
-    alt=""
-  />
-</noscript>
+        {children}
+<MetaPixel />
         <BookingModalProvider>
           <SmoothScroll>{children}</SmoothScroll>
           <BookingModal />
